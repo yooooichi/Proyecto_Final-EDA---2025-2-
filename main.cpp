dbd213ee49462ef8b8f7433b2f95e18b0f197099
joshua <unaostra@gmail.com>
2025-11-01 16:22:53 -0500

    Creacion de la clase Persona
    Implementación de getters y setters, ademas de la función crearPersona

--- main.cpp
+++ main.cpp
@@ -5,11 +5,57 @@
 #include <string>
 #include <random>
 #include <ctime>
-
 using namespace std;
+
+class Persona {
+  private:
+  string DNI;
+  string Nombres;
+  string Apellidos;
+  string Nacionalidad;
+  string Lugar_Nacimiento;
+  string Direccion;
+  string Telefono;
+  string Correo;
+  string Estado_C;
+
+  public:
+  string getDNI() { return DNI; }
+  string getNombres() { return Nombres; }
+  string getApellidos() { return Apellidos; }
+  string getNacionalidad() { return Nacionalidad; }
+  string getLugar_Nacimiento() { return Lugar_Nacimiento; }
+  string getDireccion() { return Direccion; }
+  string getTelefono() { return Telefono; }
+  string getCorreo() { return Correo; }
+  string getEstado() { return Estado_C; }
+
+  void setDNI(string x) { DNI = x; }
+  void setNombres(string x) { Nombres = x; }
+  void setApellidos(string x) { Apellidos = x; }
+  void setNacionalidad(string x) { Nacionalidad = x; }
+  void setLugar_Nacimiento(string x) { Lugar_Nacimiento = x; }
+  void setDireccion(string x) { Direccion = x; }
+  void setTelefono(string x) { Telefono = x; }
+  void setCorreo(string x) { Correo = x; }
+  void setEstado(string x) { Estado_C = x; }
+  
+  void CrearPersona(string a, string b, string c, string d, string e, string f, string g, string h, string i) {
+    setDNI(a);
+    setNombres(b);
+    setApellidos(c);
+    setNacionalidad(d);
+    setLugar_Nacimiento(e);
+    setDireccion(f);
+    setTelefono(g);
+    setCorreo(h);
+    setEstado(i);
+  }
+  
+};
 
 int main() {
 
 
-  
+  return 0;
 }
